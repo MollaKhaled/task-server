136619956258f99c21de08106681e074af3dea8f
Mollakhaled <khaledbalok@gmail.com>
2025-03-08 17:04:26 +0600

    link added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://eloquent-seahorse-6bf68c.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
